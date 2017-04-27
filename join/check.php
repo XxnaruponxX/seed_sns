64229abb9112d8597db2f48b23e2e727e5f63a9f
DESKTOP-C379KDD\kirur <kirurakiru.0524@gmail.com>
2017-04-27 11:27:32 +0800

    2回目

---
+++ join/check.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>SeedSNS</title>
+
+    <!-- Bootstrap -->
+    <link href="../../assets/css/bootstrap.css" rel="stylesheet">
+    <link href="../../assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="../../assets/css/form.css" rel="stylesheet">
+    <link href="../../assets/css/timeline.css" rel="stylesheet">
+    <link href="../../assets/css/main.css" rel="stylesheet">
+    <!--
+      designフォルダ内では2つパスの位置を戻ってからcssにアクセスしていることに注意！
+     -->
+
+  </head>
+  <body>
+  <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header page-scroll">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="index.html"><span class="strong-title"><i class="fa fa-twitter-square"></i> Seed SNS</span></a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+              </ul>
+          </div>
+          <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container-fluid -->
+  </nav>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-md-offset-4 content-margin-top">
+        <form method="post" action="" class="form-horizontal" role="form">
+          <input type="hidden" name="action" value="submit">
+          <div class="well">ご登録内容をご確認ください。</div>
+            <table class="table table-striped table-condensed">
+              <tbody>
+                <!-- 登録内容を表示 -->
+                <tr>
+                  <td><div class="text-center">ニックネーム</div></td>
+                  <td><div class="text-center">Seed kun</div></td>
+                </tr>
+                <tr>
+                  <td><div class="text-center">メールアドレス</div></td>
+                  <td><div class="text-center">seed@nex.com</div></td>
+                </tr>
+                <tr>
+                  <td><div class="text-center">パスワード</div></td>
+                  <td><div class="text-center">●●●●●●●●</div></td>
+                </tr>
+                <tr>
+                  <td><div class="text-center">プロフィール画像</div></td>
+                  <td><div class="text-center"><img src="http://c85c7a.medialib.glogster.com/taniaarca/media/71/71c8671f98761a43f6f50a282e20f0b82bdb1f8c/blog-images-1349202732-fondo-steve-jobs-ipad.jpg" width="100" height="100"></div></td>
+                </tr>
+              </tbody>
+            </table>
+
+            <a href="index.html">&laquo;&nbsp;書き直す</a> |
+            <input type="submit" class="btn btn-default" value="会員登録">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../../assets/js/jquery-3.1.1.js"></script>
+    <script src="../../assets/js/jquery-migrate-1.4.1.js"></script>
+    <script src="../../assets/js/bootstrap.js"></script>
+  </body>
+</html>
