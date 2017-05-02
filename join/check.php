4d7fc746ff88adaf02336512b968f19d7ed34844
DESKTOP-C379KDD\kirur <kirurakiru.0524@gmail.com>
2017-05-03 00:01:24 +0800

    コミット

--- join/check.php
+++ join/check.php
@@ -70,9 +70,9 @@ session_start();
                 </tr>
                 <tr>
                   <td><div class="text-center">プロフィール画像</div></td>
-                  <td><div class="text-center"><img src="http://c85c7a.medialib.glogster.com/taniaarca/media/71/71c8671f98761a43f6f50a282e20f0b82bdb1f8c/blog-images-1349202732-fondo-steve-jobs-ipad.jpg" width="100" height="100"></div></td>
-                </tr>
-              </tbody>
+                  <td><div class="text-center"><img src="../member_picture/<?php echo $_SESSION['join']['picture_path'];?>" width="100" height="100"></div></td>
+                  </tr>       
+                  </tbody>
             </table>
 
             <a href="index.html">&laquo;&nbsp;書き直す</a> |
